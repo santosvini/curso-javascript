29fe7ee60730cd37da13feb2dbe7092582efafc3
vinisantos7 <vini_santos7@live.com>
2021-06-17 17:38:54 -0300

    Alteração do código e resolução do exe!

--- aula14ex/ex018/script.js
+++ aula14ex/ex018/script.js
@@ -1,13 +1,18 @@
-function tabuada() {
-  let num = document.getElementById("txtn");
-  let tab = document.getElementById("seltab");
-  if (num.value.lenght == 0) {
-        alert("Por favor, digite um número!")
-  } else {
-    let n = Number(num.value)
-    let c = 1
-    while (c < 10) {
-        let item = document.createElement('option')
-    }
-  }
+  function tabuada() {
+      var num = document.getElementById('txtn')
+      var tab = document.getElementById('seltab')
+      if (num.value.length == 0) {
+        alert('Por favor, Digite um número!')
+      } else {
+          var n = Number(num.value)
+          var c = 1
+          tab.innerHTML = ''
+          while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+          }
+      }
 }
