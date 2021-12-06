4d98c19167308a39c4efd1e2c3bd26a68e39112c
Vini Santos <vini_santos7@live.com>
2021-12-05 23:06:40 -0300

    :octocat: change fixed on the return my response in innerHTML :octocat:

--- Desafios/d007/cambio.js
+++ Desafios/d007/cambio.js
@@ -1,8 +1,10 @@
 function cambio() {
-    let reais = prompt('Quantos R$ você tem na carteira?')
+    let reais = prompt('Quantos R$ você tem na carteira?').replace('.' , ',')
 
     let dolar = reais / cotacao_usd
 
     let resp = document.getElementById("saida")
-    resp.innerHTML = `<p>Você vai conseguir comprar ${dolar} dólares</p>`.replace('.' , ',').toLocaleString('en-us', {style: 'currency', currency: 'USD'})
+    resp.innerHTML = `<p>Você vai conseguir comprar ${dolar} dólares</p>`
+                       .toLocaleString('en-us', {style: 'currency',
+                                                 currency: 'USD'}).fixed(2)
 }
